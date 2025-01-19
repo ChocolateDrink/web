bacd6c712e6ce99641888a883ccab69d2a3fcd69
chocolate <readonlychocolate@gmail.com>
2025-01-19 01:13:26 -0800

    Add files via upload

---
+++ scriptbloxRemover/main.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         scriptblox remover
+// @version      19/1/2025
+// @description  removes the annoying prompts
+// @author       warmchocolatedrink
+// @match        *://*.scriptblox.com/*
+// @run-at       document-start
+// ==/UserScript==
+
+const toRemove = [
+	'.w-full.bg-green-500'
+];
+
+let interval;
+
+const remove = () => {
+	toRemove.forEach(query => {
+		const button = document.querySelector(query);
+		if (button) button.click();
+	});
+};
+
+const clickAnywhere = () => {
+	if (!window.location.href.includes('/script')) return;
+	document?.body?.click();
+};
+
+const start = () => {
+	if (interval) clearInterval(interval);
+	interval = setInterval(() => {
+		remove();
+		clickAnywhere();
+	}, 1);
+};
+
+window.addEventListener('hashchange', start);
+window.addEventListener('popstate', start);
+
+start();
